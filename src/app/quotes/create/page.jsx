df5f543b3536fd9a30cba73c430297fbe9cbd03f
islamBelabbes <belabbesislam2@gmail.com>
2024-01-02 17:53:44 +0100

    create quote page

---
+++ src/app/quotes/create/page.jsx
@@ -0,0 +1,12 @@
+import QuoteForm from "@/components/Quote/QuoteForm";
+import React from "react";
+
+function page() {
+  return (
+    <div className="app">
+      <QuoteForm />
+    </div>
+  );
+}
+
+export default page;
