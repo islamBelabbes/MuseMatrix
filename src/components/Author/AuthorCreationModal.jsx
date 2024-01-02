b6056d4233c236f134ae22c06d490cbf8af8079f
islamBelabbes <belabbesislam2@gmail.com>
2024-01-02 17:54:34 +0100

    refactor

---
+++ src/components/Author/AuthorCreationModal.jsx
@@ -0,0 +1,53 @@
+import Conditional from "@/components/Conditional";
+import { SingleImageUploader } from "@/components/ImageUploader";
+import Modal from "@/components/Modal/Modal";
+import Image from "next/image";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+function AuthorCreationModal({ mutate, authorName, closeModal }) {
+  const [image, setImage] = useState(null);
+
+  const deleteHandler = () => {
+    if (!image) return toast.error("الرجاء اختيار صورة");
+    closeModal();
+    mutate({ name: authorName, avatar: image });
+  };
+  return (
+    <Modal isGlobal={false} localOnClose={closeModal}>
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-[12px] items-center">
+          <div className="w-[36px] h-[36px] rounded-full border  bg-slate-400">
+            <Conditional
+              condition={image}
+              onTrue={
+                <Image
+                  src={image}
+                  alt="avatar"
+                  className="object-cover w-full h-full rounded-full"
+                  width={"36"}
+                  height={"36"}
+                />
+              }
+            />
+          </div>
+          <span className="text-base font-medium">{authorName}</span>
+          <SingleImageUploader
+            images={image}
+            setImages={setImage}
+            className={"mr-auto"}
+          />
+        </div>
+        <button
+          className="w-full button_primary"
+          disabled={!image}
+          onClick={deleteHandler}
+        >
+          انشاء
+        </button>
+      </div>
+    </Modal>
+  );
+}
+
+export default AuthorCreationModal;
---
+++ src/components/Author/AuthorSelect.jsx
@@ -0,0 +1,93 @@
+import BlockUi from "@/components/BlockUi";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AsyncCreatableSelect from "react-select/async-creatable";
+import AuthorCreationModal from "./AuthorCreationModal";
+import { toast } from "react-toastify";
+import { AnimatePresence } from "framer-motion";
+
+function AuthorSelect({ author, setAuthor }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [authorName, setAuthorName] = useState(null);
+
+  const getOptions = async (inputValue) => {
+    const response = await axios.get(`/api/author?name=${inputValue}`);
+    return response.data.data.map((item) => {
+      return { value: item.id, label: item.name, avatar: item.avatar };
+    });
+  };
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: ({ name, avatar }) => {
+      return axios.post("/api/author", { name, avatar });
+    },
+    mutationKey: ["author"],
+  });
+
+  const handleCreate = async ({ name, avatar }) => {
+    const { data } = await toast.promise(() => mutateAsync({ name, avatar }), {
+      pending: "الرجاء الانتظار",
+      success: "تم انشاء المصدر بنجاح 👌",
+      error: "حدث خطأ",
+    });
+    if (Boolean(data.success) !== true) return;
+    setAuthor({
+      value: data.data.id,
+      label: data.data.name,
+      avatar: data.data.avatar,
+    });
+  };
+
+  const handleOnCreate = (inputValue) => {
+    setIsOpen(true);
+    setAuthorName(inputValue);
+  };
+
+  const isBlock = isPending;
+
+  return (
+    <div className="flex items-center w-full gap-2">
+      {/* using normal conditional rendering for animatePresence */}
+      <AnimatePresence>
+        {isOpen && (
+          <AuthorCreationModal
+            mutate={handleCreate}
+            authorName={authorName}
+            closeModal={() => setIsOpen(false)}
+          />
+        )}
+      </AnimatePresence>
+
+      <label
+        htmlFor="author"
+        className="whitespace-nowrap min-w-[60px] w-[60px]"
+      >
+        المصدر :
+      </label>
+      <BlockUi
+        isBlock={isBlock}
+        classNames={{ container: "w-full", spinner: "rounded" }}
+      >
+        <AsyncCreatableSelect
+          placeholder="اختيار"
+          id="author"
+          className="flex-1 font-bold dark:bg-transparent"
+          classNames={{
+            menu: () => "!z-20",
+          }}
+          isClearable
+          isDisabled={isBlock}
+          isLoading={isBlock}
+          onCreateOption={handleOnCreate}
+          onChange={(e) => setAuthor(e)}
+          loadOptions={getOptions}
+          defaultOptions
+          value={author}
+        />
+      </BlockUi>
+    </div>
+  );
+}
+
+export default AuthorSelect;
