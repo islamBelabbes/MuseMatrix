bcd4fe90509b391cd07dbcdec8e76815a0c3895d
islamBelabbes <belabbesislam2@gmail.com>
2024-01-02 17:53:27 +0100

    refactor

--- src/app/quotes/page.jsx
+++ src/app/quotes/page.jsx
@@ -1,5 +1,7 @@
+import Quote from "@/components/Quote/Quote";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const QUOTES = [
@@ -11,111 +13,33 @@ const QUOTES = [
     authorAvatar:
       "https://utfs.io/f/0c4d2bfc-5cf0-477f-bdfc-92846f8f0a7b-8xaqvb.jpg",
     color: "#5f738d",
-  },
-  {
-    id: 1,
-    content:
-      "اعظم الضلال ليس من اخطاء في جواب الغاية وعاش طبقا عنه بل من اخطاء جواب الغاية وضل يعيش وفق جوابه كاطار للحياة",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
-  },
-  {
-    id: 1,
-    content:
-      "ان من يجد سببا يحيا به فإن بمقدوره غالبا ان يتحمل في سبيله كل الصعاب بأي وسيلة من الوسائل ",
-    author: "فيكتور فرانكل",
-    authorAvatar:
-      "https://utfs.io/f/45d21d84-d507-4e54-8d73-5369fc453d8d-4radlf.jpg	",
+    book: "الانسان والبحث عن معنى",
   },
 ];
 function page() {
   return (
     <div className="app">
       <ul className="grid grid-cols-[repeat(auto-fill,minmax(263px,1fr))] justify-center gap-5">
+        <li className="flex border border-black  items-center gap-5 flex-col max-h-[370px] rounded-xl py-7  justify-center">
+          <Link prefetch={false} href="/quotes/create">
+            <Image
+              alt="add new"
+              src="/add-new.png"
+              width={100}
+              height={100}
+              className="object-contain cursor-pointer"
+            />
+          </Link>
+        </li>
         {QUOTES.map((item) => (
-          <li
+          <Quote
             key={item.id}
-            className="flex items-center gap-5 flex-col max-h-[370px] rounded-xl py-7"
-            style={{ background: item.color ? item.color : "#262D33" }}
-          >
-            <div className="w-[156px] h-[156px]">
-              <Image
-                src={item?.authorAvatar}
-                alt="author avatar"
-                width={156}
-                height={156}
-                className="block object-cover w-full h-full rounded-full"
-              />
-            </div>
-            <div className="px-3 text-center text-white">
-              <p className="text-xs font-medium leading-4 line-clamp-1">
-                {item.author}
-              </p>
-              <p className="text-base font-bold leading-6 line-clamp-3">
-                {item.content}
-              </p>
-            </div>
-            <div className="flex items-center self-stretch justify-center mt-2 text-center text-white border-t">
-              <span className="mt-2">الانسان والبحث عن معنى</span>
-            </div>
-          </li>
+            author={item.author}
+            content={item.content}
+            book={item.book}
+            avatar={item.authorAvatar}
+            background={item?.color}
+          />
         ))}
       </ul>
     </div>
