26d57e0ad836f2604196aa8d918c1bf3edb8efdb
islamBelabbes <belabbesislam2@gmail.com>
2024-01-06 17:55:25 +0100

    .

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const password = request.cookies.get("mx_password")?.value;
-  if (password !== "123") {
+  if (password !== "123" && request.pathname !== "/not-found") {
     return NextResponse.redirect(new URL("/not-found", request.url));
   } else {
     return NextResponse.next();
