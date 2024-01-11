22fa6ed83c3e704b34340118ae3874ae6035191f
islamBelabbes <belabbesislam2@gmail.com>
2024-01-11 21:21:03 +0100

    error handling

--- src/app/quotes/page.jsx
+++ src/app/quotes/page.jsx
@@ -4,11 +4,6 @@ import { tryCatch } from "@/lib/utils";
 
 async function page() {
   const [quotes, error] = await tryCatch(getQuotes());
-  if (error) {
-    const newError = new Error(error);
-    newError.message = "hey";
-    throw newError;
-  }
 
   return (
     <div className="app">
