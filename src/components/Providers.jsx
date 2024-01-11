471da5ed64156f7f0dbfb15c0252ed3076eb6bcc
islamBelabbes <belabbesislam2@gmail.com>
2024-01-11 19:49:26 +0100

    add clerk provider

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -6,22 +6,25 @@ import "react-toastify/dist/ReactToastify.css";
 import { Next13ProgressBar } from "next13-progressbar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const queryClient = new QueryClient();
 
 function Providers({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <DarkModeProvider>
-        <ToastContainer />
-        <GlobalModalProvider>{children}</GlobalModalProvider>
-        <Next13ProgressBar
-          height="4px"
-          color="#0A2FFF"
-          options={{ showSpinner: true }}
-        />
-      </DarkModeProvider>
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ClerkProvider>
+        <DarkModeProvider>
+          <ToastContainer />
+          <GlobalModalProvider>{children}</GlobalModalProvider>
+          <Next13ProgressBar
+            height="4px"
+            color="#0A2FFF"
+            options={{ showSpinner: true }}
+          />
+        </DarkModeProvider>
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ClerkProvider>
     </QueryClientProvider>
   );
 }
