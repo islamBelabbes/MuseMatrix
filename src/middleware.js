274a85722f824e2ad60f8f6ed7a3fd289ebc867d
islamBelabbes <belabbesislam2@gmail.com>
2024-01-11 19:46:46 +0100

    add : authMiddleware

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+import { authMiddleware } from "@clerk/nextjs";
+import { PrivetRoutes } from "./constants/constants";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
+export default authMiddleware({
+  publicRoutes: (req) => {
+    let isPublic = true;
+    for (let route of PrivetRoutes) {
+      if (req.url.includes(route)) {
+        isPublic = false;
+      }
+    }
+    return isPublic;
+  },
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
