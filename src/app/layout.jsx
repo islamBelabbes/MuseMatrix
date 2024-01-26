391dc8052bc0d83d7040034ae3cb8f56d60f9ec9
islamBelabbes <belabbesislam2@gmail.com>
2024-01-26 13:47:56 +0100

    move clerkProvider to layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Providers from "@/components/Providers";
 import { Tajawal } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "@/globals.css";
 const tajawal = Tajawal({
@@ -17,14 +18,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className={`dark:bg-[#161513] ${tajawal.className}`}>
+      <ClerkProvider>
         <Providers>
-          <div id="modal"></div>
-          <Header />
-          <main>{children}</main>
-          <Footer />
+          <body className={`dark:bg-[#161513] ${tajawal.className}`}>
+            <div id="modal"></div>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+          </body>
         </Providers>
-      </body>
+      </ClerkProvider>
     </html>
   );
 }
--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -6,25 +6,22 @@ import "react-toastify/dist/ReactToastify.css";
 import { Next13ProgressBar } from "next13-progressbar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const queryClient = new QueryClient();
 
 function Providers({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ClerkProvider>
-        <DarkModeProvider>
-          <ToastContainer />
-          <GlobalModalProvider>{children}</GlobalModalProvider>
-          <Next13ProgressBar
-            height="4px"
-            color="#0A2FFF"
-            options={{ showSpinner: true }}
-          />
-        </DarkModeProvider>
-        <ReactQueryDevtools initialIsOpen={true} />
-      </ClerkProvider>
+      <DarkModeProvider>
+        <ToastContainer />
+        <GlobalModalProvider>{children}</GlobalModalProvider>
+        <Next13ProgressBar
+          height="4px"
+          color="#0A2FFF"
+          options={{ showSpinner: true }}
+        />
+      </DarkModeProvider>
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
