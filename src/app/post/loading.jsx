2e418eee3ae0ea06771dfa643f6307fd68a3831d
islamBelabbes <belabbesislam2@gmail.com>
2024-02-02 10:44:08 +0100

    change loader color

--- src/app/post/loading.jsx
+++ src/app/post/loading.jsx
@@ -2,8 +2,8 @@
 import { ClipLoader } from "react-spinners";
 function loading() {
   return (
-    <div className="h-screen flex justify-center">
-      <ClipLoader color="#36d7b7" />
+    <div className="flex justify-center h-screen">
+      <ClipLoader color="#4B6BFB" />
     </div>
   );
 }
