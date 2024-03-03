0b07c89bd19e30237495afe6c7f1f8bc0e6e9a8f
islamBelabbes <belabbesislam2@gmail.com>
2024-03-03 09:52:02 +0100

    empty fullback

--- src/components/AuthorAvatar.jsx
+++ src/components/AuthorAvatar.jsx
@@ -4,7 +4,7 @@ function AuthorAvatar({ image = 156, size = 156 }) {
   return (
     <Avatar style={{ width: `${size}px`, height: `${size}px` }}>
       <AvatarImage src={image} alt="author" className="object-cover" />
-      <AvatarFallback>CN</AvatarFallback>
+      <AvatarFallback></AvatarFallback>
     </Avatar>
   );
 }
