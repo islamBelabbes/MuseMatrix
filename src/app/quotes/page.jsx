782510a353ff64ef90fcc236e1b07758afbcff03
islamBelabbes <belabbesislam2@gmail.com>
2024-08-25 15:09:00 +0100

    add caching

--- src/app/quotes/page.jsx
+++ src/app/quotes/page.jsx
@@ -1,11 +1,20 @@
 import QuoteListing from "@/components/Quote/QuoteListing";
 import { getQuotes } from "@/lib/db";
+import { unstable_cache as cache } from "next/cache";
 
 export const metadata = {
   title: "Quotes",
 };
+
+const cachedGetQuotes = cache(
+  async () => getQuotes({ limit: 10, page: 1 }),
+  ["quotes"],
+  { tags: ["quotes"] }
+);
+
 async function page() {
-  const quotes = await getQuotes({ limit: 10, page: 1 });
+  const quotes = await cachedGetQuotes();
+
   return (
     <div className="app">
       <QuoteListing initialData={quotes} />
