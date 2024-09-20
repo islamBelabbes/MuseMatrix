4e3d1d074425dc8c022272b35067f7e1fcccad88
islamBelabbes <belabbesislam2@gmail.com>
2024-09-20 11:12:57 +0100

    clean up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,6 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { SITE_NAME } from "./constants";
 import { z } from "zod";
-import { TUser } from "@/dto/users";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -31,24 +30,6 @@ export function generateSearchParams(
     });
 
   return searchParams;
-}
-
-export async function urlToFile(url: string): Promise<File> {
-  // Fetch the image from the URL
-  const response = await fetch(url);
-
-  // Get the image as a Blob
-  const blob = await response.blob();
-
-  // Extract the filename from the URL
-  const filename = url.split("/").pop() ?? "file";
-
-  // Extract the file extension to determine the MIME type
-  const ext = filename.split(".").pop();
-  const mimeType = ext ? `image/${ext}` : "application/octet-stream"; // Default to 'application/octet-stream' if the type is unknown
-
-  // Create and return a new File object
-  return new File([blob], filename, { type: mimeType });
 }
 
 export const wait = (ms: number): Promise<void> => {
