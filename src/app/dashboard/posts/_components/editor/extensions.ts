2596ead506ef8269857cb7cc20bb4ad7d9ba1f1b
islamBelabbes <belabbesislam2@gmail.com>
2024-09-22 19:49:00 +0100

    Novel Editor Init

---
+++ src/app/dashboard/posts/_components/editor/extensions.ts
@@ -0,0 +1,153 @@
+"use client";
+import {
+  TiptapImage,
+  TiptapLink,
+  UpdatedImage,
+  TaskList,
+  TaskItem,
+  HorizontalRule,
+  StarterKit,
+  Placeholder,
+} from "novel/extensions";
+import { UploadImagesPlugin } from "novel/plugins";
+
+import { cx } from "class-variance-authority";
+
+import BaseHeading from "@tiptap/extension-heading";
+import { mergeAttributes } from "@tiptap/core";
+
+type Levels = 1 | 2 | 3;
+
+const classes: Record<Levels, string> = {
+  1: "text-4xl font-bold",
+  2: "text-3xl font-bold",
+  3: "text-2xl font-bold",
+};
+
+const Heading = BaseHeading.configure({ levels: [1, 2, 3] }).extend({
+  renderHTML({ node, HTMLAttributes }) {
+    const hasLevel = this.options.levels.includes(node.attrs.level);
+    const level: Levels = hasLevel ? node.attrs.level : this.options.levels[0];
+
+    return [
+      `h${level}`,
+      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes, {
+        class: `${classes[level]}`,
+      }),
+      0,
+    ];
+  },
+});
+
+const placeholder = Placeholder.configure({
+  placeholder: ({ node }) => {
+    if (node.type.name === "heading") {
+      return `Heading ${node.attrs.level}`;
+    }
+    return "استعمل / من اجل الاوامر الجاهزة";
+  },
+  includeChildren: true,
+});
+
+const tiptapLink = TiptapLink.configure({
+  HTMLAttributes: {
+    class: cx(
+      "text-muted-foreground underline underline-offset-[3px] hover:text-primary transition-colors cursor-pointer",
+    ),
+  },
+});
+
+const tiptapImage = TiptapImage.extend({
+  addProseMirrorPlugins() {
+    return [
+      UploadImagesPlugin({
+        imageClass: cx("opacity-40 rounded-lg border border-stone-200"),
+      }),
+    ];
+  },
+}).configure({
+  allowBase64: true,
+  HTMLAttributes: {
+    class: cx("rounded-lg border border-muted"),
+  },
+});
+
+const updatedImage = UpdatedImage.configure({
+  HTMLAttributes: {
+    class: cx("rounded-lg border border-muted"),
+  },
+});
+
+const taskList = TaskList.configure({
+  HTMLAttributes: {
+    class: cx("not-prose pl-2 "),
+  },
+});
+const taskItem = TaskItem.configure({
+  HTMLAttributes: {
+    class: cx("flex gap-2 items-start my-4"),
+  },
+  nested: true,
+});
+
+const horizontalRule = HorizontalRule.configure({
+  HTMLAttributes: {
+    class: cx("mt-4 mb-6 border-t border-muted-foreground"),
+  },
+});
+const heading = Heading;
+
+const starterKit = StarterKit.configure({
+  heading: false,
+  bulletList: {
+    HTMLAttributes: {
+      class: cx("list-disc list-outside leading-3 -mt-2"),
+    },
+  },
+  orderedList: {
+    HTMLAttributes: {
+      class: cx("list-decimal list-outside leading-3 -mt-2"),
+    },
+  },
+  listItem: {
+    HTMLAttributes: {
+      class: cx("leading-normal -mb-2"),
+    },
+  },
+  blockquote: {
+    HTMLAttributes: {
+      class: cx("border-r-4 border-primary"),
+    },
+  },
+  codeBlock: {
+    HTMLAttributes: {
+      class: cx(
+        "rounded-md bg-muted text-muted-foreground border p-5 font-mono font-medium",
+      ),
+    },
+  },
+  code: {
+    HTMLAttributes: {
+      class: cx("rounded-md bg-muted  px-1.5 py-1 font-mono font-medium"),
+      spellcheck: "false",
+    },
+  },
+  horizontalRule: false,
+  dropcursor: {
+    color: "#DBEAFE",
+    width: 4,
+  },
+  gapcursor: false,
+});
+
+export const defaultExtensions = [
+  starterKit,
+  tiptapLink,
+  tiptapImage,
+  updatedImage,
+  taskList,
+  taskItem,
+  horizontalRule,
+  heading,
+  placeholder,
+];
---
+++ src/app/dashboard/posts/_components/editor/slash-command.tsx
@@ -0,0 +1,162 @@
+import {
+  CheckSquare,
+  Code,
+  Heading1,
+  Heading2,
+  Heading3,
+  ImageIcon,
+  List,
+  ListOrdered,
+  MessageSquarePlus,
+  Text,
+  TextQuote,
+} from "lucide-react";
+import { createSuggestionItems } from "novel/extensions";
+import { Command, renderItems } from "novel/extensions";
+import { uploadFn } from "./image-upload";
+
+export const suggestionItems = createSuggestionItems([
+  {
+    title: "إرسال ملاحظات",
+    description: "أخبرنا كيف يمكننا التحسين.",
+    icon: <MessageSquarePlus size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).run();
+      window.open("/feedback", "_blank");
+    },
+  },
+  {
+    title: "نص",
+    description: "ابدأ الكتابة بنص عادي.",
+    searchTerms: ["p", "فقرة"],
+    icon: <Text size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode("paragraph", "paragraph")
+        .run();
+    },
+  },
+  {
+    title: "قائمة المهام",
+    description: "تتبع المهام باستخدام قائمة المهام.",
+    searchTerms: ["todo", "مهمة", "قائمة", "تحقق", "مربع اختيار"],
+    icon: <CheckSquare size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleTaskList().run();
+    },
+  },
+  {
+    title: "عنوان 1",
+    description: "عنوان قسم كبير.",
+    searchTerms: ["title", "كبير", "ضخم"],
+    icon: <Heading1 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode("heading", { level: 1 })
+        .run();
+    },
+  },
+  {
+    title: "عنوان 2",
+    description: "عنوان قسم متوسط.",
+    searchTerms: ["subtitle", "متوسط"],
+    icon: <Heading2 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode("heading", { level: 2 })
+        .run();
+    },
+  },
+  {
+    title: "عنوان 3",
+    description: "عنوان قسم صغير.",
+    searchTerms: ["subtitle", "صغير"],
+    icon: <Heading3 size={18} />,
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode("heading", { level: 3 })
+        .run();
+    },
+  },
+  {
+    title: "قائمة نقطية",
+    description: "إنشاء قائمة نقطية بسيطة.",
+    searchTerms: ["غير مرتبة", "نقطة"],
+    icon: <List size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleBulletList().run();
+    },
+  },
+  {
+    title: "قائمة مرقمة",
+    description: "إنشاء قائمة مرقمة.",
+    searchTerms: ["مرتبة"],
+    icon: <ListOrdered size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleOrderedList().run();
+    },
+  },
+  {
+    title: "اقتباس",
+    description: "التقاط اقتباس.",
+    searchTerms: ["blockquote"],
+    icon: <TextQuote size={18} />,
+    command: ({ editor, range }) =>
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .toggleNode("paragraph", "paragraph")
+        .toggleBlockquote()
+        .setNode("blockquote", { style: "direction: rtl;" }) // Add this line to set RTL direction
+        .run(),
+  },
+  {
+    title: "كود",
+    description: "التقاط كود.",
+    searchTerms: ["codeblock"],
+    icon: <Code size={18} />,
+    command: ({ editor, range }) =>
+      editor.chain().focus().deleteRange(range).toggleCodeBlock().run(),
+  },
+  {
+    title: "صورة",
+    description: "تحميل صورة من جهاز الكمبيوتر الخاص بك.",
+    searchTerms: ["photo", "صورة", "وسائط"],
+    icon: <ImageIcon size={18} />,
+    command: ({ editor, range }) => {
+      // editor.chain().focus().deleteRange(range).run();
+      // // upload image
+      // const input = document.createElement("input");
+      // input.type = "file";
+      // input.accept = "image/*";
+      // input.onchange = async () => {
+      //   if (input.files?.length) {
+      //     const file = input.files[0];
+      //     const pos = editor.view.state.selection.from;
+      //     uploadFn(file, editor.view, pos);
+      //   }
+      // };
+      // input.click();
+    },
+  },
+]);
+
+export const slashCommand = Command.configure({
+  suggestion: {
+    items: () => suggestionItems,
+    render: renderItems,
+  },
+});
