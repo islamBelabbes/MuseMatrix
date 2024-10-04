308551dc7afeed5a6134e45e9933380f2e121920
islamBelabbes <belabbesislam2@gmail.com>
2024-10-04 21:45:12 +0100

    debug

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,9 +4,13 @@ import { env } from "@/env";
 
 const createPrismaClient = () =>
   new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: ["query", "error", "warn"],
   });
+// const createPrismaClient = () =>
+//   new PrismaClient({
+//     log:
+//       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+//   });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
